9f0efd5e454db9db28ccb5db1df9326fbc647fcf
ganfanrenShijiali <mmzmam48@163.com>
2022-02-14 00:05:41 +0800

    项目目录设置

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+    export default {
+        name: 'nav-header' //组件名称或者页面名称，加载组件引用的值
+    }
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,13 @@
+//头部组件、底部组件
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'nav-header' //组件名称或者页面名称，加载组件引用的值
+    }
+</script>
