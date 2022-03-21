e4cbedfac6662d693c23622d231310c49d2b95e7
ganfanrenShijiali <mmzmam48@163.com>
2022-03-21 11:26:07 +0800

    scss预处理 vue.config.js修改 package.js修改 yarn安装

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,60 @@
     <router-view></router-view>
   </div>
 </template>
+<script>
+    // import storage from './storage'
+    export default {
+        name: 'app',
+        components: {
+
+        },
+        data() {
+            return {
+                res: {}
+            }
+        },
+        mounted() {
+            // storage.setItem('a', 1);
+            // storage.setItem('user',{a:1});
+            // storage.setItem('abc',{a:1},'user');
+            // storage.clear('a');
+            // storage.clear('a', 'user');
+            //本地加载请求静态json文件的形式
+            // this.axios.get('/mock/user/login').then((res)=>{
+            //     this.res = res;
+            // });
+            //通过easy-mock平台实现数据mock
+            // this.axios.get('/user/login').then((res)=>{
+            //     this.res = res;
+            // });
+            //本地集成mockjs实现数据Mock
+            this.axios.get('/user/login').then((res) => {
+                this.res = res;
+            });
+
+        }
+    }
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+    
+    #nav {
+        padding: 30px;
+    }
+    
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+    
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -5,16 +5,16 @@ module.exports = { //导出一个object对象
         port: 8080, //通过这里修改vue.config.js端口
         proxy: {
             '/api': {
-                target: '', //拦截到api代理到哪里去
-                changeOrigin: false, //是否将主机头的源点更改为目标点的url地址
+                target: 'http://mall-pre.springboot.cn', //拦截到api代理到哪里去
+                changeOrigin: true, //是否将主机头的源点更改为目标点的url地址
                 //访问-a实际上是访问的-b。访问的是一个网址，看起来是访问自己的接口
                 pathReWrite: {
                     'api': ''
                 }
-            },
-            '/foo': {
-                target: "<other_url>"
             }
+            // '/foo': {
+            //     target: "<other_url>"
+            // }
         }
     }
 }
