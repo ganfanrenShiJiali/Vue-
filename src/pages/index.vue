7512edd1d59c2d416a3cc577ea91c781323ad074
ganfanrenShijiali <mmzmam48@163.com>
2022-03-22 19:41:11 +0800

    首页广告位 手机商品静态完成

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -143,14 +143,40 @@
                     <div class="swiper-button-next" slot="button-next"></div>
                 </swiper>
             </div>
-            <div class="ads">
-                
+            <div class="ads-box">
+                <a v-bind:href="'/#/product/'+item.id" v-for="(item,index) in adsList" v-bind:key="index">
+                    <img v-bind:src="item.img" alt="">
+                </a>
             </div>
             <div class="banner">
-
+                <a href="/#/product/30">
+                    <img src="/imgs/banner-1.png" alt="">
+                </a>
             </div>
-            <div class="product-box">
-
+        </div>
+        <div class="product-box">
+            <div class="container">
+                <h2>手机</h2>
+                <div class="wrapper">
+                    <div class="banner-left">
+                            <a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt=""></a>
+                    </div>
+                    <div class="list-box">
+                        <div class="list" v-for="(arr,i) in phoneList" v-bind:key="i">
+                            <div class="item" v-for="(item,j) in arr" v-bind:key="j">
+                                <span>新品</span>
+                                <div class="item-img">
+                                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/0d4b362431de4bdda03315ffdbc7b32a.jpg?thumb=1&w=260&h=260&f=webp&q=90" alt="">
+                                </div>
+                                <div class="item-info">
+                                    <h3>Xiaomi 11 青春活力版</h3>
+                                    <p>骁龙855，索尼4800万超广角微距</p>
+                                    <p class="price">2999元</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <service-bar></service-bar>
@@ -242,6 +268,24 @@
                         }
                     ],
                     [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+                ],
+                adsList: [
+                    {
+                        id: 33,
+                        img: '/imgs/ads/ads-1.png'
+                    },{
+                        id: 48,
+                        img: '/imgs/ads/ads-2.jpg'
+                    },{
+                        id: 45,
+                        img: '/imgs/ads/ads-3.png'
+                    },{
+                        id: 47,
+                        img: '/imgs/ads/ads-4.jpg'
+                    }
+                ],
+                phoneList: [
+                    [0,0,0,0],[0,0,0,0]
                 ]
             }
         }
@@ -332,6 +376,89 @@
                 }
             }
         }
+        .ads-box {
+            margin-top: 14px;
+            margin-bottom: 31px;
+            @include flex();
+            a {
+                width: 296px;
+                height: 167px;
 
+            }
+        }
+        .banner {
+            margin-bottom: 50px;
+        }
+        .product-box {
+            background-color: $colorJ;
+            padding: 30px 0 50px;
+            h2 {
+                font-size: $fontF;
+                height: 21px;
+                line-height: 21px;
+                color: $colorB;
+                margin-bottom: 20px;
+            }
+            .wrapper {
+                display: flex;//弹性布局，里面的元素自动排列
+                .banner-left {
+                    margin-right: 16px;
+                    img {
+                        width: 224px;
+                        height: 619px;
+                    }
+                }
+                .list-box {
+                    .list {
+                        @include flex();
+                        width: 986px;
+                        margin-bottom: 14px;
+                        &:last-child {
+                            margin-bottom: 0;
+                        }
+                        .item {
+                            width: 236px;
+                            height: 302px;
+                            background-color: $colorG;
+                            text-align: center;
+                            span {
+
+                            }
+                            .item-img {
+                                img {
+                                    height: 195px;
+                                }
+                            }
+                            .item-info {
+                                h3 {
+                                    font-size: $fontJ;
+                                    color: $colorB;
+                                    line-height: 14px;
+                                    font-weight: bold; 
+                                }
+                                p {
+                                    color: $colorD;
+                                    line-height: 13px;
+                                    margin: 6px 0 13px;
+                                }
+                                .price {
+                                    color: #F20A0A;
+                                    font-size: $fontJ;
+                                    font-weight: bold;
+                                    cursor: pointer;
+                                    &:after {
+                                        content:' ';
+                                        @include bgImg(22px, 22px, '/imgs/icon-cart-hover.png');
+                                        margin-left: 5px;
+                                        vertical-align: text-bottom;
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 </style>
