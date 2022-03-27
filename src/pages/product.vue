00978403e871e4c5c4119dc0d9f54d6eddc9ccd4
ganfanrenShijiali <mmzmam48@163.com>
2022-03-27 22:46:07 +0800

    产品站参数组件静态

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,10 +1,27 @@
 <template>
-    <div>
-        product
+    <div class="product">
+        <product-param>
+            <template v-slot:buy>
+                <div class="btn">
+                    立即购买
+                </div>
+            </template>
+        </product-param>
     </div>
 </template>
 <script>
+    import ProductParam from './../components/ProductParam';
     export default {
-        name: 'product' //组件名称或者页面名称，加载组件引用的值
+        name: 'product', //组件名称或者页面名称，加载组件引用的值
+        components: {
+            ProductParam
+        }
     }
 </script>
+<style lang="scss">
+    .product {
+        .btn {
+            margin-left: 10px;
+        }
+    }
+</style>
